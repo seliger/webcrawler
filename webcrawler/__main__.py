764a3b077b41bd202076e2021d68c349c9ac59dd
Corey S. Seliger <corey@seligerfamily.org>
2019-04-05 10:23:30 -0400

    Main runtime handler
    Reworked to explictly kick of the runner thread inside crawler

--- webcrawler/__main__.py
+++ webcrawler/__main__.py
@@ -1,20 +1,19 @@
 """This is the WebCrawler main entry point."""
 
-from wclib.options import Options
+from wclib.options import Options 
 from wclib.config import Configuration
 from wclib.crawler import WebCrawler
-from wclib.model import DataModelConfig
+
 
 def launch_webcrawler():
     """ Actual point of execution for launching the web crawler."""
     options = Options().params
     config = Configuration(options)
 
-    # Bootstrap the data model with the database
-    DataModelConfig(config)
-
     # Bootstrap the web crawler with the configuration
-    WebCrawler(config)
+    crawler = WebCrawler(config)
+
+    crawler.run(config.scan.seed_url)
 
 
 if __name__ == '__main__':
